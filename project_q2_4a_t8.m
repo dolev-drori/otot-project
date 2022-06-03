fc6946d31bfe3cbd0bbb2b93028c3b67a4a9ffb6
dolev <106162213+dolev-drori@users.noreply.github.com>
2022-06-03 15:43:34 +0300

    Update project_q2_4a_t8.m

--- project_q2_4a_t8.m
+++ project_q2_4a_t8.m
@@ -5,10 +5,11 @@ X_1 = fft(X1);
 y = fftshift(X_1);
 Y = abs(y);
 fs =2*pi/4;
-df = fs/ length(X1);
-fn = fs/2;
-figure();
-f = -fn:df:fn-df;
-plot(f,Y,'r');
+%df = fs/ length(X1);
+%fn = fs/2;
+%figure();
+%f = -fn:df:fn-df;
+%plot(f,Y,'r');
+plot(Y,'r');
 xlabel('frequency [HZ]');
 ylabel('DTFT')
